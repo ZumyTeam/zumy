fe4267a1672ee7a28e872207329df3cc4631a4b3
Zumy Developer Team <dev@zumy.co>
2018-09-27 17:10:49 +0100

    test velocity 2

--- src/fork.h
+++ src/fork.h
@@ -14,10 +14,9 @@ static const int64_t nReservePhaseStart = 1;
 /** Reserve Phase end block */ 
 static const int64_t nReservePhaseEnd = 3; // 
 /** Velocity toggle block */
-static const int64_t VELOCITY_TOGGLE = 120; // Implementation of the Velocity system into the chain.
-static const int64_t VELOCITY_TOGGLE = 999999999999; // Disabled the Implementation of the Velocity system into the chain. Will be added on possibly in the future :)
+static const int64_t VELOCITY_TOGGLE = 999999999999; // Implementation of the Velocity system into the chain.
 /** Velocity retarget toggle block */
-static const int64_t VELOCITY_TDIFF = 0; // Use Velocity's retargetting method.
+static const int64_t VELOCITY_TDIFF = 999999999999; // Use Velocity's retargetting method.
 /** Protocol toggle */
 inline bool IsProtocolV3(int64_t nTime) { return TestNet() || nTime > 1493596800; } // Mon, 01 May 2017 00:00:00 GMT
 
